ce352fa4038fd79ef6d7dc68d6e7317b1ca96e37
muslmasan <muslmasan3@gmai.com>
2024-09-15 16:35:33 +0200

    add another test

--- app/app/test.py
+++ app/app/test.py
@@ -7,3 +7,7 @@ class ClacTests(SimpleTestCase):
     def test_add_numbers(self):
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
+    
+    def test_substract_numbers(self):
+        res = calc.subtract(10, 15)
+        self.assertEqual(res,5)
