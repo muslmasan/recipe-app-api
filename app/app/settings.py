09a4803a33f5cac0aa9a5961f7cb962dada1aadd
muslmasan <muslmasan3@gmai.com>
2024-09-17 06:19:24 +0200

    adding some package

--- app/app/settings.py
+++ app/app/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path,os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_HOST'),
+        'USER': os.environ.get('DB_NAME'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
