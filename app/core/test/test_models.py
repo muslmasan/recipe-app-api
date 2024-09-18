32fb14b13e71bf9247f1c56976bc1ec959cf26df
muslmasan <muslmasan3@gmai.com>
2024-09-18 13:33:54 +0200

    updating the test

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -7,7 +7,9 @@ class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         email = 'test@t.com'
         password = 'testpass123'
-        user = get_user_model(
+        username = 'test'
+        user = get_user_model().objects.create_user(
+            username = username,
             email = email,
             password = password,
         )
