fe1b57abae5ea0e28c73403adceb7e0482afa788
muslmasan <muslmasan3@gmai.com>
2024-09-19 16:51:27 +0200

    editing the admin page

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -9,10 +9,34 @@ class ModelTests(TestCase):
         password = 'testpass123'
         username = 'test'
         user = get_user_model().objects.create_user(
-            username = username,
+            name = username,
             email = email,
             password = password,
         )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        sample_emails = [
+            ['test1@EXAMPLE.com',"test1@example.com"],
+            ['Test2@Example.com',"Test2@example.com"],
+            ['Test3@EXAMPLE.COM ',"Test3@example.com"],
+            ['test4@example.COM',"test4@example.com"],
+        ]
+        for email, expectd in sample_emails:
+            user = get_user_model().objects.create_user(email,'sample123')
+            self.assertEqual(user.email, expectd)
+        
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('','test123')
+
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
