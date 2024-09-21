b552fd96a3638def642501e32b85c2fd1cc3154e
muslmasan <muslmasan3@gmai.com>
2024-09-21 13:29:14 +0200

    solve flake8

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -5,33 +5,33 @@ from django.contrib.auth import (
 from django.utils.translation import gettext as _
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = get_user_model()
         fields = ['email', 'password', 'name']
-        extra_kwargs = {'password':{'write_only': True, 'min_length': 5}}
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
         def create(self, validated_data):
             return get_user_model().objects.create_user(**validated_data)
 
         def update(self, instance, validate_data):
             """Update and return user."""
-            password = validate_data.pop('password',None)
+            password = validate_data.pop('password', None)
             user = super().update(instance, validate_data)
             if password:
                 user.set_password(password)
                 user.save()
-            
-            return user
 
+            return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth token."""
     email = serializers.EmailField()
     password = serializers.CharField(
-        style={'input_type':'password'}, 
+        style={'input_type': 'password'},
         trim_whitespace=False
     )
 
@@ -40,13 +40,13 @@ class AuthTokenSerializer(serializers.Serializer):
         email = attrs.get('email')
         password = attrs.get('password')
         user = authenticate(
-            request= self.context.get('request'),
+            request=self.context.get('request'),
             username=email,
             password=password,
         )
         if not user:
             msg = _('Unable to authenticate with provided credentials.')
             raise serializers.ValidationError(msg, code='authrization')
-        
+
         attrs['user'] = user
         return attrs
--- app/user/test/test_user_api.py
+++ app/user/test/test_user_api.py
@@ -10,6 +10,7 @@ CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
 ME_URL = reverse('user:me')
 
+
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
 
@@ -45,7 +46,7 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short_error(self):
-        payload ={
+        payload = {
             'email': 'test@example.com',
             'password': 'pw',
             'name': 'Test name',
@@ -53,7 +54,7 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
-            email = payload['email']
+            email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
 
@@ -78,14 +79,14 @@ class PublicUserApiTests(TestCase):
     def test_create_token_bad_credentials(self):
         create_user(email='test@example.com', password='goodpass')
 
-        payload = {'email': 'test@example.com','password': 'badpass'}
+        payload = {'email': 'test@example.com', 'password': 'badpass'}
         res = self.client.post(TOKEN_URL, payload)
 
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_token_blank_password(self):
-        payload = {'email': 'test@example.com', 'password':''}
+        payload = {'email': 'test@example.com', 'password': ''}
         res = self.client.post(TOKEN_URL, payload)
 
         self.assertNotIn('token', res.data)
@@ -95,6 +96,7 @@ class PublicUserApiTests(TestCase):
         res = self.client.get(ME_URL)
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
 
 class PrivateUserApiTests(TestCase):
     """Test API requests that require authentications."""
@@ -115,7 +117,7 @@ class PrivateUserApiTests(TestCase):
             'email': self.user.email,
             'name': self.user.name
         })
-    
+
     def test_post_me_not_allowed(self):
         """Test Post is not allowed for the me endpoint"""
         res = self.client.post(ME_URL, {})
