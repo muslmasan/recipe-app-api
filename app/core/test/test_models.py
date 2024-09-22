bfc2a0ffbebda4d2a44c2e51490d195de55e03fc
muslmasan <muslmasan3@gmai.com>
2024-09-22 11:45:47 +0200

    test code

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -6,6 +6,10 @@ from decimal import Decimal
 from core import models
 
 
+def create_user(email='user@gmail.com', password='testpass123'):
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -59,3 +63,9 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
