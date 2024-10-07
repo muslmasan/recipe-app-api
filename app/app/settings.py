48a00c4f33d341112ab577f67d7d69bacb3c315c
muslmasan <muslmasan3@gmai.com>
2024-10-07 17:28:58 +0200

    adding the proxy dockerfile

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%*+a(%tcf2di7fhply3n4-vz43t(0ecvgkz#p79lrm$=_w%+rs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [ ]
 
 
 # Application definition
